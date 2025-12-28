9ad7b3bf246ea33dc63b2df5db1145e313cc9711
244g1a05dy-boop <244g1a05dy@srit.ac.in>
2025-12-28 13:08:33 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+class Rectangle{
+  double length;
+  double breath;
+
+   double area(){
+      return length*breath;
+   }
+
+   double perimeter(){
+   return 2*(length*braeth);
+  }
+ }
+ 
+
+
+
+
